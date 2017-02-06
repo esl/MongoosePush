449c7fd7404d7695c23e390b7024ab7a8442e128
Rafał Słota <rafal.slota@erlang-solutions.com>
2017-02-06 13:22:59 +0100

    Init repository with empty Elixir project

---
+++ lib/mongoose_push.ex
@@ -0,0 +1,18 @@
+defmodule MongoosePush do
+  @moduledoc """
+  Documentation for MongoosePush.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MongoosePush.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mongoose_push_test.exs
@@ -0,0 +1,8 @@
+defmodule MongoosePushTest do
+  use ExUnit.Case
+  doctest MongoosePush
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
