2fa8907c60d41f362bfcfeed021967a131d28be0
Rafał Słota <rafal.slota@erlang-solutions.com>
2018-04-19 13:06:19 +0200

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MongoosePush.Mixfile do
   def project do
     [
       app: :mongoose_push,
-      version: "0.11.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
