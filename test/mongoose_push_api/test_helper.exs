502e611890eb4a0c1d4ad2a13d19656bf3f8d46a
zofpolkowska <zofpolkowska@gmail.com>
2019-11-05 14:46:02 +0100

    Put tests for MongoosePush API into a separate directory

--- test/mongoose_push_api/test_helper.exs
+++ test/mongoose_push_api/test_helper.exs
@@ -1,34 +1,9 @@
-defmodule MongoosePushAPIV2Test do
-  require Logger
-  use ExUnit.Case, async: false
-  use Quixir
+ExUnit.start(capture_log: true)
+
+defmodule MongoosePushAPITestHelper do
   alias HTTPoison.Response
-  doctest MongoosePush.API.V2
 
-  @url "/v2/notification/f534534543"
-
-  setup do
-    reset(:fcm)
-    reset(:apns)
-    TestHelper.reload_app()
-  end
-
-  test "push to fcm with unknown token fails" do
-    args = %{
-      :service => :fcm,
-      :alert => %{
-        :title => "title value",
-        :body => "body value",
-        :click_action => "click.action",
-        :tag => "tag value"}}
-
-
-    mock_fcm([%{device_token: "f534534543", status: 404, reason: "UNREGISTERED"}])
-
-    assert {500, "UNREGISTERED"} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
-  end
-
-  defp post(path, json) do
+  def post(path, json) do
     %Response{status_code: status_code, body: body} =
       HTTPoison.post!(
         "https://localhost:8443" <> path,
@@ -41,7 +16,17 @@ defmodule MongoosePushAPIV2Test do
     {status_code, details}
   end
 
-  defp mock_fcm(json) do
+  def mock_apns(json) do
+    {:ok, conn} = get_connection(:apns)
+    payload = Poison.encode!(json)
+
+    headers = headers("POST", "/error-tokens", payload)
+    :h2_client.send_request(conn, headers, payload)
+    get_response(conn)
+    :ok
+  end
+
+  def mock_fcm(json) do
     {:ok, conn} = get_connection(:fcm)
     payload = Poison.encode!(json)
 
@@ -51,7 +36,7 @@ defmodule MongoosePushAPIV2Test do
     :ok
   end
 
-  defp reset(:apns) do
+  def reset(:apns) do
     {:ok, conn} = get_connection(:apns)
     headers = headers("POST", "/reset")
     :h2_client.send_request(conn, headers, "")
@@ -59,7 +44,7 @@ defmodule MongoosePushAPIV2Test do
     :ok
   end
 
-  defp reset(:fcm) do
+  def reset(:fcm) do
     {:ok, conn} = get_connection(:fcm)
     headers = headers("POST", "/mock/reset")
     :h2_client.send_request(conn, headers, "")
@@ -67,15 +52,15 @@ defmodule MongoosePushAPIV2Test do
     :ok
   end
 
-  defp get_connection(:apns) do
+  def get_connection(:apns) do
     :h2_client.start_link(:https, 'localhost', 2197, [])
   end
 
-  defp get_connection(:fcm) do
+  def get_connection(:fcm) do
     :h2_client.start_link(:https, 'localhost', 4000, [])
   end
 
-  defp headers(method, path, payload \\ "") do
+  def headers(method, path, payload \\ "") do
     [
       {":method", method},
       {":authority", "localhost"},
@@ -86,7 +71,7 @@ defmodule MongoosePushAPIV2Test do
     ]
   end
 
-  defp get_response(conn) do
+  def get_response(conn) do
     receive do
       {:END_STREAM, stream_id} ->
         {:ok, {_headers, body}} = :h2_client.get_response(conn, stream_id)
@@ -94,9 +79,6 @@ defmodule MongoosePushAPIV2Test do
     end
   end
 
-  defp push(token, notification), do: MongoosePush.push(token, notification)
-
 end
-
 
 
---
+++ test/mongoose_push_api/v2_test.exs
@@ -0,0 +1,116 @@
+defmodule MongoosePushAPIV2Test do
+  require Logger
+  use ExUnit.Case, async: false
+  use Quixir
+  import MongoosePushAPITestHelper
+
+  alias HTTPoison.Response
+  doctest MongoosePush.API.V2
+
+  @url "/v2/notification/f534534543"
+
+  setup do
+    reset(:fcm)
+    reset(:apns)
+    TestHelper.reload_app()
+  end
+
+  test "push to fcm with unregistered token fails" do
+    reason = "UNREGISTERED"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 404, reason: reason}])
+
+    assert {500, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+
+  test "push to fcm with id mismatch fails" do
+    reason = "SENDER_ID_MISMATCH"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 403, reason: reason}])
+
+    assert {500, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+
+  test "push to fcm with the limit exceeded fails" do
+    reason = "QUOTA_EXCEEDED"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 429, reason: reason}])
+
+    assert {500, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+
+  test "push to fcm fails with unknown internal error" do
+    reason = "INTERNAL"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 500, reason: reason}])
+
+    assert {500, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+
+  test "push to fcm with invalid or missing certificate/web push fails" do
+    reason = "THIRD_PARTY_AUTH_ERROR"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 401, reason: reason}])
+
+    assert {500, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+
+  test "push to fcm fails when service is unavailable/overloaded" do
+    reason = "UNAVAILABLE"
+    args = %{
+      :service => :fcm,
+      :alert => %{
+        :title => "title value",
+        :body => "body value",
+        :click_action => "click.action",
+        :tag => "tag value"}}
+
+
+    mock_fcm([%{device_token: "f534534543", status: 503, reason: reason}])
+
+    assert {503, reason} = post(@url, %{service: :fcm, alert: %{body: "body", title: "title"}})
+  end
+end
+
+
+
