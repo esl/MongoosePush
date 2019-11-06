06f61f20a4ddca60aa459399ea556ddf8ee7cc9b
zofpolkowska <zofpolkowska@gmail.com>
2019-11-06 13:13:50 +0100

    Add test/support/api.ex to .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,5 +2,6 @@
   ~r/unmatched_return/,
   ~r/Function :asn1ct.compile\/2 does not exist/,
   ~r/lib\/mix\//,
-  {"lib/mongoose_push/router.ex"}
+  {"lib/mongoose_push/router.ex"},
+  {"test/support/api.ex"}
 ]
