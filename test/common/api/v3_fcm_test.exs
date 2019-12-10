048efc46256fefc05a13c2e56ee7b0252da3d9a1
zofpolkowska <zofpolkowska@gmail.com>
2019-12-10 12:31:53 +0100

    Rework the request to the FCM mock

--- test/common/api/v3_fcm_test.exs
+++ test/common/api/v3_fcm_test.exs
@@ -10,15 +10,16 @@ defmodule MongoosePush.API.V3FCMTest do
     end
 
     Tools.reset(:fcm)
+    :ok
   end
 
   @tag integration: true
   test "push to fcm with id mismatch fails" do
     reason = "SENDER_ID_MISMATCH"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 403, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 403, reason: reason}])
 
-    assert {503, %{"reason" => "service_internal"}} =
+    assert {503, %{"reason" => "service_internal"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -26,9 +27,9 @@ defmodule MongoosePush.API.V3FCMTest do
   test "push to fcm with unregistered token fails" do
     reason = "UNREGISTERED"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 404, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 404, reason: reason}])
 
-    assert {410, %{"reason" => "unregistered"}} =
+    assert {410, %{"reason" => "unregistered"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -36,9 +37,9 @@ defmodule MongoosePush.API.V3FCMTest do
   test "push to fcm with the limit exceeded fails" do
     reason = "QUOTA_EXCEEDED"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 429, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 429, reason: reason}])
 
-    assert {429, %{"reason" => "too_many_requests"}} =
+    assert {429, %{"reason" => "too_many_requests"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -46,9 +47,9 @@ defmodule MongoosePush.API.V3FCMTest do
   test "push to fcm fails with unknown internal error" do
     reason = "INTERNAL"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 500, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 500, reason: reason}])
 
-    assert {503, %{"reason" => "service_internal"}} =
+    assert {503, %{"reason" => "service_internal"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -56,9 +57,9 @@ defmodule MongoosePush.API.V3FCMTest do
   test "push to fcm with invalid or missing certificate/web push fails" do
     reason = "THIRD_PARTY_AUTH_ERROR"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 401, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 401, reason: reason}])
 
-    assert {503, %{"reason" => "service_internal"}} =
+    assert {503, %{"reason" => "service_internal"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -66,9 +67,9 @@ defmodule MongoosePush.API.V3FCMTest do
   test "push to fcm fails when service is unavailable/overloaded" do
     reason = "UNAVAILABLE"
 
-    Tools.mock_fcm([%{device_token: "f534534543", status: 503, reason: reason}])
+    Tools.mock_fcm("/error-tokens", [%{device_token: "f534534543", status: 503, reason: reason}])
 
-    assert {503, %{"reason" => "service_internal"}} =
+    assert {503, %{"reason" => "service_internal"}} ==
              Tools.post(@url, Tools.sample_notification(:fcm))
   end
 
@@ -78,6 +79,6 @@ defmodule MongoosePush.API.V3FCMTest do
 
     Tools.reset(:fcm)
 
-    assert {200, _} = Tools.post(@url, Tools.sample_notification(:fcm))
+    assert {200, nil} == Tools.post(@url, Tools.sample_notification(:fcm))
   end
 end
