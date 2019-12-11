a6d3f7843d2723d8f88f8ebc0555a04c9e6cc8b0
zofpolkowska <zofpolkowska@gmail.com>
2019-12-11 16:03:21 +0100

    Move TimeHelper to a separate module

--- test/common/test_helper.exs
+++ test/common/test_helper.exs
@@ -2,22 +2,6 @@ ExUnit.start(capture_log: true)
 
 HTTPoison.start()
 
-defmodule TimeHelper do
-  def wait_until(fun), do: wait_until(500, fun)
-
-  def wait_until(0, fun), do: fun.()
-
-  def wait_until(timeout, fun) do
-    try do
-      fun.()
-    rescue
-      ExUnit.AssertionError ->
-        :timer.sleep(10)
-        wait_until(max(0, timeout - 10), fun)
-    end
-  end
-end
-
 defmodule TestHelper do
   @fcm_http_port 4001
   def reload_app() do
--- test/unit/mogoose_push_metrics_test.exs
+++ test/unit/mogoose_push_metrics_test.exs
@@ -3,7 +3,7 @@ defmodule MongoosePushMetricsTest do
   use Quixir
   import MongoosePush
   import Mock
-  import TimeHelper
+  import MongoosePush.Support.TimeHelper
 
   alias MongoosePush.Service.APNS
   alias MongoosePush.Service.FCM
