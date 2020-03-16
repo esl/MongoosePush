6964bf0238671bb70fd1cfb67d89cb9f7f608002
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2020-03-16 13:22:09 +0100

    Update Phoenix and dialyxir

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule MongoosePush.Mixfile do
       {:meck, github: "eproxus/meck", override: true},
       {:httpoison, "~> 1.6.2"},
       {:excoveralls, "~> 0.7", only: :test},
-      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false},
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:ex_doc, "~> 0.14", only: :dev},
       {:quixir, "~> 0.9", only: [:test, :integration]},
