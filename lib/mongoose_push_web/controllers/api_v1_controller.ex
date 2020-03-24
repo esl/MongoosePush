94640988d20144ba65ff5588af5191ac38929268
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-03-24 18:48:47 +0100

    add OpenAPI spec for APIv1 - initial version

---
+++ lib/mongoose_push_web/controllers/api_v1_controller.ex
@@ -0,0 +1,34 @@
+defmodule MongoosePushWeb.APIv1Controller do
+  alias MongoosePushWeb.Schemas
+  alias OpenApiSpex.Operation
+  use MongoosePushWeb, :controller
+
+  @spec open_api_operation(atom) :: Operation.t()
+  def open_api_operation(action) do
+    operation = String.to_existing_atom("#{action}_operation")
+    apply(__MODULE__, operation, [])
+  end
+
+  @spec handle_operation() :: Operation.t()
+  def handle_operation() do
+    %Operation{
+      tags: ["apiv1"],
+      summary: "sends a push",
+      description: "performs the sending of push notification",
+      operationId: "APIv1Controller.handle",
+      parameters: [
+        Operation.parameter(:id, :path, :string, "Device ID", example: "f53453455", required: true)
+      ],
+      requestBody:
+        Operation.request_body(
+          "The push notification attributes",
+          "application/json",
+          Schemas.APIv1Request,
+          required: true
+        ),
+      responses: %{
+        200 => Operation.response("PushNotification", "application/json", Schemas.APIv1Response)
+      }
+    }
+  end
+end
---
+++ lib/mongoose_push_web/schemas.ex
@@ -0,0 +1,36 @@
+defmodule MongoosePushWeb.Schemas do
+  alias OpenApiSpex.Schema
+
+  defmodule APIv1Request do
+    require OpenApiSpex
+
+    OpenApiSpex.schema(%{
+      title: "User",
+      description: "A user of the app",
+      type: :object,
+      properties: %{
+        service: %Schema{type: :string, description: "Push notification service", format: :string},
+        body: %Schema{type: :string, description: "Body of the notification", format: :string},
+        title: %Schema{type: :string, description: "Title of the notification", format: :string}
+      },
+      required: [:service, :body, :title],
+      example: %{
+        "service" => "apns",
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    })
+  end
+
+  defmodule APIv1Response do
+    require OpenApiSpex
+
+    OpenApiSpex.schema(%{
+      title: "APIv1Response",
+      description: "Response schema for push notification request",
+      type: :object,
+      properties: %{},
+      example: %{}
+    })
+  end
+end
