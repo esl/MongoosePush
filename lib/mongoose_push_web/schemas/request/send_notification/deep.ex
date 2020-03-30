9a19272c2060c31fe5bc5694f2c2b0a5dff18bc7
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-03-30 15:22:54 +0200

    Request.SendNotification for APIv2/APIv3 - initial version

---
+++ lib/mongoose_push_web/schemas/request/send_notification/deep.ex
@@ -0,0 +1,45 @@
+defmodule MongoosePushWeb.Schemas.Request.SendNotification.Deep do
+  require OpenApiSpex
+  alias OpenApiSpex.Schema
+
+  OpenApiSpex.schema(%{
+    title: "Request.SendNotification.Deep",
+    description: "Push notification request schema",
+    type: :object,
+    properties: %{
+      service: %Schema{
+        type: :string,
+        description: "Push notification service",
+        format: :string,
+        enum: ["fcm", "apns"]
+      },
+      mode: %Schema{type: :string, enum: [:prod, :dev]},
+      priority: %Schema{type: :string, enum: [:normal, :high]},
+      time_to_live: %Schema{type: :integer, format: :int32},
+      mutable_content: %Schema{type: :boolean, default: false},
+      tags: %Schema{type: &Deep.tags_helper/1, default: []},
+      # Only for APNS, alert/data independent
+      topic: %Schema{type: :string},
+      alert: %Schema{
+        type: :object,
+        properties: %{
+          body: %Schema{type: :string, description: "Body of the notification", format: :string},
+          title: %Schema{type: :string, description: "Title of the notification", format: :string},
+          badge: %Schema{type: :integer, format: :int32},
+          click_action: %Schema{type: :string},
+          tag: %Schema{type: :string},
+          sound: %Schema{type: :string}
+        },
+        required: [:body, :title]
+      },
+      data: %Schema{type: :string}
+    },
+    required: [:service, :alert],
+    example: %{
+      "service" => "apns",
+      "alert" => %{"body" => "A message from someone", "title" => "Notification title"}
+    }
+  })
+
+  def tags_helper(tags), do: Enum.map(tags, &String.to_existing_atom(&1))
+end
