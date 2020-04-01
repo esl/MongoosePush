11efbe9e9f2b52c534c97fde31c6666101eee0fb
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-04-01 18:41:59 +0200

    more response codes for APIv1, PayloadOnly -> GenericError

--- lib/mongoose_push_web/schemas/response/send_notification/payload_only.ex
+++ lib/mongoose_push_web/schemas/response/send_notification/payload_only.ex
@@ -1,11 +1,19 @@
-defmodule MongoosePushWeb.Schemas.Response.SendNotification.PayloadOnly do
+defmodule MongoosePushWeb.Schemas.Response.SendNotification.GenericError do
   require OpenApiSpex
+  alias OpenApiSpex.Schema
 
   OpenApiSpex.schema(%{
-    title: "APIv1.Response.Push",
+    title: "Response.SendNotification.GenericError",
     description: "Response schema for push notification request",
     type: :object,
-    properties: %{},
+    properties: %{
+      details: %Schema{
+        type: :string,
+        description:
+          "Short description of the encountered error. For development/debugging purposes only, " <>
+            "not for any client usage, as it may change in time without prior notice."
+      }
+    },
     example: %{}
   })
 end
