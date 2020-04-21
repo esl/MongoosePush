d8eb480ea1dd4c6d645289e17539c25ba0397d2c
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-04-21 20:08:34 +0200

    fix dialyzer

--- lib/mongoose_push_behaviour.ex
+++ lib/mongoose_push_behaviour.ex
@@ -1,5 +1,5 @@
 defmodule MongoosePushBehaviour do
   @moduledoc false
   @callback push(String.t(), MongoosePush.request()) ::
-              :ok | {:error, Service.error()} | {:error, MongoosePush.error()}
+              :ok | {:error, MongoosePush.Service.error()} | {:error, MongoosePush.error()}
 end
