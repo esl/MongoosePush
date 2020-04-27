38b572f4aa35cf6488c23dbc96d9eba694793b9c
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-04-27 17:58:37 +0200

    Turn away from macros, prefer regular functions

--- lib/mongoose_push/metrics/exometer.ex
+++ lib/mongoose_push/metrics/exometer.ex
@@ -1,6 +1,6 @@
 defmodule MongoosePush.Metrics.Exometer do
   @moduledoc """
-  This module provides some utility macros that simplify the use of Elixometer.
+  This module provides some utility functions that simplify the use of Elixometer.
   """
 
   defmacro __using__(_opts) do
@@ -16,66 +16,53 @@ defmodule MongoosePush.Metrics.Exometer do
   Provided return value of `:ok` is counted as succeses, while
   `{:error, reason :: term}` as error `reason`.
   """
-  defmacro update_success(return_value, mtype, metric, value \\ 1) do
-    quote bind_quoted: [mtype: mtype, metric: metric, value: value, return_value: return_value],
-          unquote: true do
-      alias MongoosePush.Metrics.Exometer, as: Metrics
+  def update_success(return_value, mtype, metric, value \\ 1) do
+    alias MongoosePush.Metrics.Exometer, as: Metrics
 
-      final_metrics = [Metrics.name(unquote(mtype), unquote(metric), [:success])]
+    final_metrics = [Metrics.name(mtype, metric, [:success])]
 
-      for final_metric <- final_metrics do
-        Metrics.update_metric(unquote(mtype), final_metric, unquote(value))
+    for final_metric <- final_metrics do
+      Metrics.update_metric(mtype, final_metric, value)
+    end
+
+    return_value
+  end
+
+  def update_error(return_value, mtype, metric, value \\ 1) do
+    alias MongoosePush.Metrics.Exometer, as: Metrics
+
+    {:error, reason} = return_value
+
+    general_metric = Metrics.name(mtype, metric, [:error, :all])
+
+    main_metric =
+      case is_atom(reason) do
+        true ->
+          Metrics.name(mtype, metric, [:error, reason])
+
+        false ->
+          Metrics.name(mtype, metric, [:error, :unknown])
       end
 
-      return_value
+    final_metrics = [main_metric, general_metric]
+
+    for final_metric <- final_metrics do
+      Metrics.update_metric(mtype, final_metric, value)
     end
+
+    return_value
   end
 
-  defmacro update_error(return_value, mtype, metric, value \\ 1) do
-    quote bind_quoted: [mtype: mtype, metric: metric, value: value, return_value: return_value],
-          unquote: true do
-      alias MongoosePush.Metrics.Exometer, as: Metrics
-
-      {:error, reason} = return_value
-
-      general_metric = Metrics.name(unquote(mtype), unquote(metric), [:error, :all])
-
-      main_metric =
-        case is_atom(reason) do
-          true ->
-            Metrics.name(unquote(mtype), unquote(metric), [:error, reason])
-
-          false ->
-            Metrics.name(unquote(mtype), unquote(metric), [:error, :unknown])
-        end
-
-      final_metrics = [main_metric, general_metric]
-
-      for final_metric <- final_metrics do
-        Metrics.update_metric(unquote(mtype), final_metric, unquote(value))
-      end
-
-      return_value
-    end
+  def update_metric(:spiral, metric, value) do
+    Elixometer.update_spiral(metric, value)
   end
 
-  defmacro update_metric(:spiral, metric, value) do
-    quote bind_quoted: [metric: metric, value: value], unquote: true do
-      Elixometer.update_spiral(metric, value)
-    end
+  def update_metric(:timer, metric, value) do
+    Elixometer.Updater.timer(metric, :microsecond, value)
   end
 
-  defmacro update_metric(:timer, metric, value) do
-    quote bind_quoted: [metric: metric, value: value], unquote: true do
-      Elixometer.Updater.timer(metric, :microsecond, value)
-    end
-  end
-
-  defmacro name(type, prefix, suffix) do
-    quote bind_quoted: [type: type, prefix: prefix, suffix: suffix],
-          unquote: true do
-      unquote(List.flatten([:mongoose_push, :"#{type}s", prefix, suffix]))
-      |> Enum.map(&Atom.to_string/1)
-    end
+  def name(type, prefix, suffix) do
+    List.flatten([:mongoose_push, :"#{type}s", prefix, suffix])
+    |> Enum.map(&Atom.to_string/1)
   end
 end
