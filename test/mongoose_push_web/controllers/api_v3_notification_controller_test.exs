657c68df1204975ca545cc063a91a9b7d0738c87
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-05-08 16:47:17 +0200

    add tests for APIv2 & APIv3

--- test/mongoose_push_web/controllers/api_v3_notification_controller_test.exs
+++ test/mongoose_push_web/controllers/api_v3_notification_controller_test.exs
@@ -134,4 +134,455 @@ defmodule MongoosePushWeb.APIv3NotificationControllerTest do
 
     assert json_response(conn, 422) == ControllersHelper.no_schemas_provided_response()
   end
+
+  # Service.error() errors
+
+  test "invalid request error", %{conn: conn} do
+    post_and_assert_error(conn, 400, {:invalid_request, :BadCollapseId}, :invalid_request)
+  end
+
+  test "unregistered error", %{conn: conn} do
+    post_and_assert_error(conn, 410, {:unregistered, :Unregistered}, :unregistered)
+  end
+
+  test "payload too large error", %{conn: conn} do
+    post_and_assert_error(conn, 413, {:payload_too_large, :PayloadTooLarge}, :payload_too_large)
+  end
+
+  test "too many requests error", %{conn: conn} do
+    post_and_assert_error(conn, 429, {:too_many_requests, :TooManyRequests}, :too_many_requests)
+  end
+
+  test "service internal error", %{conn: conn} do
+    post_and_assert_error(conn, 503, {:service_internal, :ServiceUnavailable}, :service_internal)
+  end
+
+  test "auth error", %{conn: conn} do
+    post_and_assert_error(conn, 503, {:auth, :BadCertificate}, :service_internal)
+  end
+
+  test "internal config error", %{conn: conn} do
+    post_and_assert_error(conn, 503, {:internal_config, :DuplicateHeaders}, :internal_config)
+  end
+
+  test "unspecified error", %{conn: conn} do
+    post_and_assert_error(conn, 520, {:unspecified, :Unspecified}, :unspecified)
+  end
+
+  # MongoosePush.error() errors
+
+  test "generic no matching pool error", %{conn: conn} do
+    post_and_assert_error(conn, 400, {:generic, :no_matching_pool}, :no_matching_pool)
+  end
+
+  test "generic unable to connect error", %{conn: conn} do
+    post_and_assert_error(conn, 503, {:generic, :unable_to_connect}, :unable_to_connect)
+  end
+
+  test "generic connection lost error", %{conn: conn} do
+    post_and_assert_error(conn, 503, {:generic, :connection_lost}, :connection_lost)
+  end
+
+  test "generic invalid notification error", %{conn: conn} do
+    post_and_assert_error(conn, 400, {:generic, :invalid_notification}, :invalid_notification)
+  end
+
+  test "generic unspecified error", %{conn: conn} do
+    post_and_assert_error(conn, 500, {:generic, :unspecified}, :unspecified)
+  end
+
+  # decoder end-to-end tests
+
+  test "AlertNotification/SilentNotification decoder test: all possible fields", %{conn: conn} do
+    device_id = "1"
+    expected_device_id = "1"
+
+    request = %{
+      "service" => "apns",
+      "mode" => "prod",
+      "priority" => "normal",
+      "time_to_live" => 3600,
+      "mutable_content" => false,
+      "tags" => ["some", "tags", "for", "pool", "selection"],
+      "topic" => "com.someapp",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title",
+        "badge" => 7,
+        "click_action" => ".SomeApp.Handler.action",
+        "tag" => "info",
+        "sound" => "standard.mp3"
+      },
+      "data" => %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected_request = %{
+      alert: %{
+        badge: 7,
+        body: "A message from someone",
+        click_action: ".SomeApp.Handler.action",
+        sound: "standard.mp3",
+        tag: "info",
+        title: "Notification title"
+      },
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      mode: :prod,
+      service: :apns,
+      topic: "com.someapp",
+      priority: :normal,
+      time_to_live: 3600,
+      mutable_content: false,
+      tags: ["some", "tags", "for", "pool", "selection"]
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: all required fields", %{conn: conn} do
+    device_id = "2"
+    expected_device_id = "2"
+
+    request = %{
+      "service" => "fcm",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      service: :fcm,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: alert.badge + alert.click_action fields", %{conn: conn} do
+    device_id = "3"
+    expected_device_id = "3"
+
+    request = %{
+      "service" => "apns",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title",
+        "badge" => 7,
+        "click_action" => ".SomeApp.Handler.action"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        badge: 7,
+        body: "A message from someone",
+        click_action: ".SomeApp.Handler.action",
+        title: "Notification title"
+      },
+      service: :apns,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: alert.tag + alert.sound fields", %{conn: conn} do
+    device_id = "4"
+    expected_device_id = "4"
+
+    request = %{
+      "service" => "fcm",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title",
+        "tag" => "info",
+        "sound" => "standard.mp3"
+      },
+      "data" => %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        sound: "standard.mp3",
+        tag: "info",
+        title: "Notification title"
+      },
+      service: :apns,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: time_to_live + mutable_content fields", %{conn: conn} do
+    device_id = "5"
+    expected_device_id = "5"
+
+    request = %{
+      "service" => "apns",
+      "time_to_live" => 3600,
+      "mutable_content" => true,
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      service: :apns,
+      time_to_live: 3600,
+      mutable_content: true
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: tags + topic fields", %{conn: conn} do
+    device_id = "6"
+    expected_device_id = "6"
+
+    request = %{
+      "service" => "apns",
+      "tags" => ["some", "tags", "for", "pool", "selection"],
+      "topic" => "com.someapp",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      service: :apns,
+      topic: "com.someapp",
+      mutable_content: false,
+      tags: ["some", "tags", "for", "pool", "selection"]
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: mode :prod + priority :high fields", %{conn: conn} do
+    device_id = "7"
+    expected_device_id = "7"
+
+    request = %{
+      "service" => "fcm",
+      "mode" => "prod",
+      "priority" => "high",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      mode: :prod,
+      service: :fcm,
+      priority: :high,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: mode :dev + priority :normal fields", %{conn: conn} do
+    device_id = "8"
+    expected_device_id = "8"
+
+    request = %{
+      "service" => "apns",
+      "mode" => "dev",
+      "priority" => "normal",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title"
+      }
+    }
+
+    expected_request = %{
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      mode: :dev,
+      service: :apns,
+      priority: :normal,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "AlertNotification decoder test: data + alert.badge field", %{conn: conn} do
+    device_id = "9"
+    expected_device_id = "9"
+
+    request = %{
+      "service" => "apns",
+      "alert" => %{
+        "body" => "A message from someone",
+        "title" => "Notification title",
+        "badge" => 7
+      },
+      "data" => %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected_request = %{
+      alert: %{
+        badge: 7,
+        body: "A message from someone",
+        title: "Notification title"
+      },
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      service: :apns,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "SilentNotification decoder test: all required fields", %{conn: conn} do
+    device_id = "10"
+    expected_device_id = "10"
+
+    request = %{
+      "service" => "apns",
+      "data" => %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected_request = %{
+      service: :apns,
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "SilentNotification decoder test: time_to_live + mutable_content fields", %{conn: conn} do
+    device_id = "11"
+    expected_device_id = "11"
+
+    request = %{
+      "service" => "apns",
+      "data" => %{"acme1" => "value1", "acme2" => "value2"},
+      "time_to_live" => 3600,
+      "mutable_content" => true
+    }
+
+    expected_request = %{
+      service: :apns,
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      time_to_live: 3600,
+      mutable_content: true
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "SilentNotification decoder test: tags + topic fields", %{conn: conn} do
+    device_id = "12"
+    expected_device_id = "12"
+
+    request = %{
+      "service" => "apns",
+      "data" => %{"acme1" => "value1", "acme2" => "value2"},
+      "tags" => ["some", "tags", "for", "pool", "selection"],
+      "topic" => "com.someapp"
+    }
+
+    expected_request = %{
+      service: :apns,
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      topic: "com.someapp",
+      mutable_content: false,
+      tags: ["some", "tags", "for", "pool", "selection"]
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "SilentNotification decoder test: mode :prod + priority :high fields", %{conn: conn} do
+    device_id = "13"
+    expected_device_id = "13"
+
+    request = %{
+      "service" => "fcm",
+      "data" => %{"acme1" => "value1", "acme2" => "value2"},
+      "mode" => "prod",
+      "priority" => "high"
+    }
+
+    expected_request = %{
+      mode: :prod,
+      service: :fcm,
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      priority: :high,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  test "SilentNotification decoder test: mode :dev + priority :normal fields", %{conn: conn} do
+    device_id = "14"
+    expected_device_id = "14"
+
+    request = %{
+      "service" => "apns",
+      "data" => %{"acme1" => "value1", "acme2" => "value2"},
+      "mode" => "dev",
+      "priority" => "normal"
+    }
+
+    expected_request = %{
+      mode: :dev,
+      service: :apns,
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      priority: :normal,
+      mutable_content: false
+    }
+
+    post_and_assert(conn, device_id, expected_device_id, request, expected_request)
+  end
+
+  defp post_and_assert(conn, device_id, expected_device_id, request, expected_request) do
+    expect(MongoosePush.Notification.MockImpl, :push, fn device_id, request ->
+      assert request == expected_request
+      assert device_id == expected_device_id
+      :ok
+    end)
+
+    post(conn, "/v3/notification/#{device_id}", Jason.encode!(request))
+  end
+
+  defp post_and_assert_error(conn, number, {type, reason}, error_reason) do
+    device_id = "666"
+    request = ControllersHelper.alert_request()
+
+    expect(MongoosePush.Notification.MockImpl, :push, fn _device_id, _request ->
+      {:error, {type, reason}}
+    end)
+
+    conn = post(conn, "/v3/notification/#{device_id}", Jason.encode!(request))
+
+    assert json_response(conn, number) == %{"reason" => to_string(error_reason)}
+  end
 end
--- test/unit/request_decoder_test.exs
+++ test/unit/request_decoder_test.exs
@@ -1,10 +1,13 @@
 defmodule RequestDecoderTest do
   use ExUnit.Case, async: false
   alias MongoosePushWeb.Protocols.RequestDecoder
-  alias MongoosePushWeb.Schemas.Request
+  alias MongoosePushWeb.Schemas.Request.SendNotification.Deep.AlertNotification
+  alias MongoosePushWeb.Schemas.Request.SendNotification.Deep.SilentNotification
+  alias MongoosePushWeb.Schemas.Request.SendNotification.Deep.Common.Alert
+  alias MongoosePushWeb.Schemas.Request.SendNotification.FlatNotification
 
-  test "decoder does well with all-fields schema" do
-    input = %Request.SendNotification.FlatNotification{
+  test "FlatNotification: decoder does well with all-fields schema" do
+    input = %FlatNotification{
       badge: 7,
       body: "A message from someone",
       click_action: ".SomeApp.Handler.action",
@@ -33,8 +36,8 @@ defmodule RequestDecoderTest do
     assert expected == RequestDecoder.decode(input)
   end
 
-  test "decoder does not fail without optional alert fields" do
-    input = %Request.SendNotification.FlatNotification{
+  test "FlatNotification: decoder does not fail without optional alert fields" do
+    input = %FlatNotification{
       body: "A message from someone",
       service: "apns",
       title: "Notification title",
@@ -57,8 +60,8 @@ defmodule RequestDecoderTest do
     assert expected == RequestDecoder.decode(input)
   end
 
-  test "decoder does not fail without optional fields at all" do
-    input = %Request.SendNotification.FlatNotification{
+  test "FlatNotification: decoder does not fail without optional fields at all" do
+    input = %FlatNotification{
       body: "A message from someone",
       service: "apns",
       title: "Notification title"
@@ -70,6 +73,204 @@ defmodule RequestDecoderTest do
         body: "A message from someone",
         title: "Notification title"
       }
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  # Alert
+
+  test "Alert: minimum fields schema" do
+    input = %Alert{
+      body: "A message from someone",
+      title: "Notification title"
+    }
+
+    expected = %{
+      body: "A message from someone",
+      title: "Notification title"
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  test "Alert: all-fields schema" do
+    input = %Alert{
+      body: "A message from someone",
+      title: "Notification title",
+      badge: 7,
+      click_action: ".SomeApp.Handler.action",
+      tag: "info",
+      sound: "standard.mp3"
+    }
+
+    expected = %{
+      body: "A message from someone",
+      title: "Notification title",
+      badge: 7,
+      click_action: ".SomeApp.Handler.action",
+      tag: "info",
+      sound: "standard.mp3"
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  test "Alert: badge + click_action" do
+    input = %Alert{
+      body: "A message from someone",
+      title: "Notification title",
+      badge: 7,
+      click_action: ".SomeApp.Handler.action"
+    }
+
+    expected = %{
+      body: "A message from someone",
+      title: "Notification title",
+      badge: 7,
+      click_action: ".SomeApp.Handler.action"
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  test "Alert: tag + sound" do
+    input = %Alert{
+      body: "A message from someone",
+      title: "Notification title",
+      tag: "info",
+      sound: "standard.mp3"
+    }
+
+    expected = %{
+      body: "A message from someone",
+      title: "Notification title",
+      tag: "info",
+      sound: "standard.mp3"
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  # AlertNotification
+
+  test "AlertNotification: decoder does well with all-fields schema" do
+    input = %AlertNotification{
+      service: "apns",
+      mode: "prod",
+      priority: "normal",
+      time_to_live: 3600,
+      mutable_content: true,
+      tags: ["some", "tags", "for", "pool", "selection"],
+      topic: "com.someapp",
+      alert: %Alert{
+        body: "A message from someone",
+        title: "Notification title",
+        badge: 7,
+        click_action: ".SomeApp.Handler.action",
+        tag: "info",
+        sound: "standard.mp3"
+      },
+      data: %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected = %{
+      service: :apns,
+      mode: :prod,
+      priority: :normal,
+      time_to_live: 3600,
+      mutable_content: true,
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title",
+        badge: 7,
+        tag: "info",
+        click_action: ".SomeApp.Handler.action",
+        sound: "standard.mp3"
+      },
+      topic: "com.someapp",
+      data: %{"acme1" => "value1", "acme2" => "value2"},
+      tags: ["some", "tags", "for", "pool", "selection"]
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  test "AlertNotification: minimum fields schema" do
+    input = %AlertNotification{
+      service: "fcm",
+      alert: %Alert{
+        body: "A message from someone",
+        title: "Notification title"
+      }
+    }
+
+    expected = %{
+      service: :fcm,
+      mutable_content: false,
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title"
+      }
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  # SilentNotification
+
+  test "SilentNotification: decoder does well with all-fields schema" do
+    input = %SilentNotification{
+      service: "apns",
+      mode: "prod",
+      priority: "normal",
+      time_to_live: 3600,
+      mutable_content: true,
+      tags: ["some", "tags", "for", "pool", "selection"],
+      topic: "com.someapp",
+      alert: %Alert{
+        body: "A message from someone",
+        title: "Notification title",
+        badge: 7,
+        click_action: ".SomeApp.Handler.action",
+        tag: "info",
+        sound: "standard.mp3"
+      },
+      data: %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected = %{
+      service: :apns,
+      mode: :prod,
+      priority: :normal,
+      time_to_live: 3600,
+      mutable_content: true,
+      tags: ["some", "tags", "for", "pool", "selection"],
+      topic: "com.someapp",
+      alert: %{
+        body: "A message from someone",
+        title: "Notification title",
+        badge: 7,
+        tag: "info",
+        click_action: ".SomeApp.Handler.action",
+        sound: "standard.mp3"
+      },
+      data: %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    assert expected == RequestDecoder.decode(input)
+  end
+
+  test "SilentNotification: minimum fields schema" do
+    input = %SilentNotification{
+      service: "fcm",
+      data: %{"acme1" => "value1", "acme2" => "value2"}
+    }
+
+    expected = %{
+      service: :fcm,
+      mutable_content: false,
+      data: %{"acme1" => "value1", "acme2" => "value2"}
     }
 
     assert expected == RequestDecoder.decode(input)
