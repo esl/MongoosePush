29a6e0699f013c729cd56a8bda2444bf391af450
Rafał Słota <rafal.slota@erlang-solutions.com>
2020-05-26 00:39:31 +0200

    Reenable bind_addr setting

--- lib/mongoose_push/config/confex_provider.ex
+++ lib/mongoose_push/config/confex_provider.ex
@@ -11,4 +11,18 @@ defmodule MongoosePush.Config.ConfexProvider do
       Confex.resolve_env!(app)
     end)
   end
+
+  @doc """
+  Used by `prod.exs` to parse env variables to inet-style IP addresses
+  """
+  def parse_bind_addr(string_addr) do
+    case :inet.parse_address(String.to_charlist(string_addr)) do
+      {:ok, value} ->
+        {:ok, value}
+
+      {:error, reason} ->
+        # Confex requires reason to be string
+        {:error, inspect(reason)}
+    end
+  end
 end
