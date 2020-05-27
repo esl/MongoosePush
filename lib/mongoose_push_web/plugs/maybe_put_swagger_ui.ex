5d74d3acc2a14ba4dd1fca637f1435ce4d695caa
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-05-27 16:17:38 +0200

    add more logic for proper enabling/disabling

--- lib/mongoose_push_web/plugs/maybe_put_swagger_ui.ex
+++ lib/mongoose_push_web/plugs/maybe_put_swagger_ui.ex
@@ -1,13 +1,30 @@
 defmodule MongoosePushWeb.Plug.MaybePutSwaggerUI do
+  import Plug.Conn
   @behaviour Plug
 
   @impl Plug
   def init(opts) do
-    OpenApiSpex.Plug.SwaggerUI.init(opts)
+    enable =
+      Application.get_env(:mongoose_push, :openapi)[:expose_spec] &&
+        Application.get_env(:mongoose_push, :openapi)[:expose_ui]
+
+    opts =
+      if enable do
+        OpenApiSpex.Plug.SwaggerUI.init(opts)
+      end
+
+    %{enabled: enable, opts: opts}
   end
 
   @impl Plug
-  def call(conn, opts) do
+  def call(conn, %{enabled: enable, opts: opts}) when enable == false or opts == nil do
+    conn
+    |> send_resp(404, "swaggerUI disabled")
+    |> halt()
+  end
+
+  @impl Plug
+  def call(conn, %{opts: opts}) do
     OpenApiSpex.Plug.SwaggerUI.call(conn, opts)
   end
 end
--- lib/mongoose_push_web/plugs/maybe_render_spec.ex
+++ lib/mongoose_push_web/plugs/maybe_render_spec.ex
@@ -1,13 +1,28 @@
 defmodule MongoosePushWeb.Plug.MaybeRenderSpec do
+  import Plug.Conn
   @behaviour Plug
 
   @impl Plug
   def init(opts) do
-    OpenApiSpex.Plug.RenderSpec.init(opts)
+    enable = Application.get_env(:mongoose_push, :openapi)[:expose_spec]
+
+    opts =
+      if enable do
+        OpenApiSpex.Plug.RenderSpec.init(opts)
+      end
+
+    %{enabled: enable, opts: opts}
   end
 
   @impl Plug
-  def call(conn, opts) do
+  def call(conn, %{enabled: enable, opts: opts}) when enable == false or opts == nil do
+    conn
+    |> send_resp(404, "swagger.json access disabled")
+    |> halt()
+  end
+
+  @impl Plug
+  def call(conn, %{opts: opts}) do
     OpenApiSpex.Plug.RenderSpec.call(conn, opts)
   end
 end
