dd782f2b0fe2539bfaf7601d671e754a3a41308e
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-06-02 15:31:48 +0200

    cover corner cases

--- lib/mongoose_push_web/plugs/cast_and_validate.ex
+++ lib/mongoose_push_web/plugs/cast_and_validate.ex
@@ -1,6 +1,5 @@
 defmodule MongoosePushWeb.Plug.CastAndValidate do
   @behaviour Plug
-  alias Plug.Conn
 
   @impl Plug
   def init(opts) do
@@ -8,52 +7,31 @@ defmodule MongoosePushWeb.Plug.CastAndValidate do
   end
 
   @impl Plug
-  def call(
-        conn = %{private: %{open_api_spex: private_data}, params: %{"service" => _, "alert" => _}},
-        opts = %{
-          operation_id: operation_id,
-          render_error: render_error
-        }
-      ) do
+  def call(conn = %{params: %{"service" => _, "alert" => _}}, opts = %{operation_id: _}) do
     OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
   end
 
-  def call(
-        conn = %{private: %{open_api_spex: private_data}, params: %{"service" => _, "data" => _}},
-        opts = %{
-          operation_id: operation_id,
-          render_error: render_error
-        }
-      ) do
+  @impl Plug
+  def call(conn = %{params: %{"service" => _, "data" => _}}, opts = %{operation_id: _}) do
     OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
   end
 
-  def call(
-        conn = %{private: %{open_api_spex: private_data}, params: %{"service" => _}},
-        opts = %{
-          operation_id: operation_id,
-          render_error: render_error
-        }
-      ) do
-    conn =
-      Kernel.update_in(
-        conn,
-        [
-          Access.key(:private),
-          :open_api_spex,
-          :operation_lookup,
-          operation_id,
-          Access.key(:requestBody),
-          Access.key(:content),
-          "application/json",
-          Access.key(:schema)
-        ],
-        fn %OpenApiSpex.Schema{oneOf: [h | _]} -> h end
-      )
-
-    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
+  @impl Plug
+  def call(conn = %{params: %{"service" => _}}, opts = %{operation_id: _}) do
+    update_schema_and_do_call(conn, opts)
   end
 
+  @impl Plug
+  def call(conn = %{params: %{"alert" => _}}, opts = %{operation_id: _}) do
+    update_schema_and_do_call(conn, opts)
+  end
+
+  @impl Plug
+  def call(conn = %{params: %{"data" => _}}, opts = %{operation_id: _}) do
+    update_schema_and_do_call(conn, opts)
+  end
+
+  @impl Plug
   def call(
         conn = %{
           private: %{
@@ -92,7 +70,64 @@ defmodule MongoosePushWeb.Plug.CastAndValidate do
     end
   end
 
+  @impl Plug
   def call(conn, opts) do
+    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
+  end
+
+  # When OpenApiSpex matches incoming request to schema defined as `oneOf: [%Reference{}]`, 
+  # and the request matches more than one alternative schema, it likes to throw misleading error
+  # "Failed to cast value to one of: [] (no schemas provided)", instead of something more meaningful.
+  # Here we avoid it by manual modification of the pattern the request is matched against,
+  # so the framework can serve us as expected.
+  defp update_schema_and_do_call(
+         conn = %{params: %{"data" => _}},
+         opts = %{operation_id: operation_id}
+       ) do
+    conn =
+      Kernel.update_in(
+        conn,
+        [
+          Access.key(:private),
+          :open_api_spex,
+          :operation_lookup,
+          operation_id,
+          Access.key(:requestBody),
+          Access.key(:content),
+          "application/json",
+          Access.key(:schema)
+        ],
+        fn %OpenApiSpex.Schema{oneOf: _listOfSchemas} ->
+          %OpenApiSpex.Reference{
+            "$ref": "#/components/schemas/Request.SendNotification.Deep.SilentNotification"
+          }
+        end
+      )
+
+    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
+  end
+
+  defp update_schema_and_do_call(conn, opts = %{operation_id: operation_id}) do
+    conn =
+      Kernel.update_in(
+        conn,
+        [
+          Access.key(:private),
+          :open_api_spex,
+          :operation_lookup,
+          operation_id,
+          Access.key(:requestBody),
+          Access.key(:content),
+          "application/json",
+          Access.key(:schema)
+        ],
+        fn %OpenApiSpex.Schema{oneOf: _listOfSchemas} ->
+          %OpenApiSpex.Reference{
+            "$ref": "#/components/schemas/Request.SendNotification.Deep.AlertNotification"
+          }
+        end
+      )
+
     OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
   end
 end
