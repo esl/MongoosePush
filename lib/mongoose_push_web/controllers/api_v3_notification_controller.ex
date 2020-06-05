0340931568960eda5bf7e822e35723ace1205753
Leszek Witkowicz <leszek.witkowicz@erlang-solutions.com>
2020-06-05 16:24:52 +0200

    new implementation of CastAndValidate plug

--- lib/mongoose_push_web/controllers/api_v3_notification_controller.ex
+++ lib/mongoose_push_web/controllers/api_v3_notification_controller.ex
@@ -5,7 +5,7 @@ defmodule MongoosePushWeb.APIv3.NotificationController do
 
   use MongoosePushWeb.Schemas
 
-  plug(MongoosePushWeb.Plug.NewCastAndValidate)
+  plug(MongoosePushWeb.Plug.CastAndValidate)
 
   @spec send_operation() :: Operation.t()
   def send_operation() do
--- lib/mongoose_push_web/plugs/cast_and_validate.ex
+++ lib/mongoose_push_web/plugs/cast_and_validate.ex
@@ -1,7 +1,7 @@
 defmodule MongoosePushWeb.Plug.CastAndValidate do
   @moduledoc """
   Module plug that serves as a wrapper for OpenApiSpex.Plug.CastAndValidate plug,
-  to overcome difficulties with proper message validating. For more details, 
+  to overcome difficulties with proper message validating. For more details,
   please refer to update_schema_and_do_call/2 function comment.
   """
   @behaviour Plug
@@ -12,80 +12,24 @@ defmodule MongoosePushWeb.Plug.CastAndValidate do
   end
 
   @impl Plug
-  def call(conn = %{params: %{"service" => _, "alert" => _}}, opts = %{operation_id: _}) do
-    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
-  end
+  def call(conn, opts) do
+    stub_conn = %Plug.Conn{
+      conn
+      | adapter: {MongoosePushWeb.Plug.CastAndValidateStubAdapter, %{}}
+    }
 
-  @impl Plug
-  def call(conn = %{params: %{"service" => _, "data" => _}}, opts = %{operation_id: _}) do
-    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
-  end
+    # Just a dry run with stub adapter so that response can't be sent
+    result = OpenApiSpex.Plug.CastAndValidate.call(stub_conn, opts)
 
-  @impl Plug
-  def call(conn = %{params: %{"service" => _}}, opts = %{operation_id: _}) do
-    update_schema_and_do_call(conn, opts)
-  end
-
-  @impl Plug
-  def call(conn = %{params: %{"alert" => _}}, opts = %{operation_id: _}) do
-    update_schema_and_do_call(conn, opts)
-  end
-
-  @impl Plug
-  def call(conn = %{params: %{"data" => _}}, opts = %{operation_id: _}) do
-    update_schema_and_do_call(conn, opts)
-  end
-
-  @impl Plug
-  def call(conn = %{params: %{}}, opts = %{operation_id: _}) do
-    update_schema_and_do_call(conn, opts)
-  end
-
-  @impl Plug
-  def call(
-        conn = %{
-          private: %{
-            phoenix_controller: controller,
-            phoenix_action: action,
-            open_api_spex: private_data
-          }
-        },
-        opts
-      ) do
-    operation =
-      case private_data.operation_lookup[{controller, action}] do
-        nil ->
-          operationId = controller.open_api_operation(action).operationId
-          operation = private_data.operation_lookup[operationId]
-
-          operation_lookup =
-            private_data.operation_lookup
-            |> Map.put({controller, action}, operation)
-
-          OpenApiSpex.Plug.Cache.adapter().put(
-            private_data.spec_module,
-            {private_data.spec, operation_lookup}
-          )
-
-          operation
-
-        operation ->
-          operation
-      end
-
-    if operation.operationId do
-      call(conn, Map.put(opts, :operation_id, operation.operationId))
+    # Now we run the plug on original, unmodified conn, but with conditional schema change
+    if result.status == 422 do
+      update_schema_and_do_call(conn, Map.put(opts, :operation_id, get_operation(conn)))
     else
-      raise "operationId was not found in action API spec"
+      OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
     end
   end
 
-  @impl Plug
-  def call(conn, opts) do
-    OpenApiSpex.Plug.CastAndValidate.call(conn, opts)
-  end
-
-  # When OpenApiSpex matches incoming request to schema defined as `oneOf: [%Reference{}]`, 
+  # When OpenApiSpex matches incoming request to schema defined as `oneOf: [%Reference{}]`,
   # and the request matches more than one alternative schema, it likes to throw misleading error
   # "Failed to cast value to one of: [] (no schemas provided)", instead of something more meaningful.
   # Here we avoid it by manual modification of the pattern the request is matched against,
@@ -114,20 +58,23 @@ defmodule MongoosePushWeb.Plug.CastAndValidate do
   end
 
   defp update_schema(conn, operation_id, new_schema) do
-    conn =
-      Kernel.update_in(
-        conn,
-        [
-          Access.key(:private),
-          :open_api_spex,
-          :operation_lookup,
-          operation_id,
-          Access.key(:requestBody),
-          Access.key(:content),
-          "application/json",
-          Access.key(:schema)
-        ],
-        fn %OpenApiSpex.Schema{oneOf: _listOfSchemas} -> new_schema end
-      )
+    Kernel.update_in(
+      conn,
+      [
+        Access.key(:private),
+        :open_api_spex,
+        :operation_lookup,
+        operation_id,
+        Access.key(:requestBody),
+        Access.key(:content),
+        "application/json",
+        Access.key(:schema)
+      ],
+      fn %OpenApiSpex.Schema{oneOf: _listOfSchemas} -> new_schema end
+    )
+  end
+
+  def get_operation(_conn = %{private: %{phoenix_controller: controller, phoenix_action: action}}) do
+    controller.open_api_operation(action).operationId
   end
 end
