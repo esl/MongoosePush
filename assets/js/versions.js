1cc82d6e2f240a5dcaa9d4582acfd8cde2e3d367
zofpolkowska <zofpolkowska@gmail.com>
2020-06-26 09:42:26 +0200

    Add versions list file

---
+++ assets/js/versions.js
@@ -0,0 +1,30 @@
+var versionNodes = [
+    {
+      version: "v2.1.0-alpha.1", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.1.0-alpha.1/introduction.html"
+    },
+    {
+      version: "v2.0.2", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.0.2/readme.html#content"
+    },
+    {
+      version: "v2.0.1", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.0.1/readme.html#content"
+    },
+    {
+      version: "v2.0.0-beta.1", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.0.0-beta.1/readme.html#content"
+    },
+    {
+      version: "v2.0.0-alpha.4", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.0.0-alpha.4/readme.html#content"
+    },
+    {
+      version: "v2.0.0", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v2.0.0/readme.html#content"
+    },
+    {
+      version: "v1.0.6", // version number or name (required)
+      url: "https://esl.github.io/MongoosePush/v1.0.6/readme.html#content"
+    }
+]
