a71330243227474b19a0c022a4850f9f15ca48d8
Krzysztof Makieła <krzysztof.makiela@erlang-solutions.com>
2020-07-01 14:05:05 +0200

    Change response structure to match the suggestions of the RFC

--- lib/mongoose_push_web/controllers/healthcheck_controller.ex
+++ lib/mongoose_push_web/controllers/healthcheck_controller.ex
@@ -4,11 +4,13 @@ defmodule MongoosePushWeb.HealthcheckController do
   def send(conn = %Plug.Conn{}, %{}) do
     stats = :wpool.stats()
 
-    payload =
+    connections =
       stats
       |> Enum.map(&extract_connection_info_from_pool/1)
 
-    status = get_status(payload)
+    status = get_status(connections)
+
+    payload = format_response(status, connections)
 
     conn
     |> put_status(status)
@@ -21,31 +23,26 @@ defmodule MongoosePushWeb.HealthcheckController do
     {_, sup_pid, _, _} = List.keyfind(children, [:wpool_process_sup], 3)
     workers = Supervisor.which_children(sup_pid)
 
-    connections =
+    {_workers, connection_status} =
       workers
-      |> Enum.map(fn worker_info ->
+      |> Enum.map_reduce(%{connected: 0, disconnected: 0}, fn worker_info, acc ->
         {_, worker_pid, _, _} = worker_info
 
         if Sparrow.H2Worker.is_alive_connection(worker_pid) do
-          :connected
+          {worker_info, Map.update!(acc, :connected, &(&1 + 1))}
         else
-          :disconnected
+          {worker_info, Map.update!(acc, :disconnected, &(&1 + 1))}
         end
       end)
 
     %{
-      pool: pool[:pool],
-      connection_status: connections
+      pool_name: pool[:pool],
+      connection_status: connection_status
     }
   end
 
   defp get_status(connections) do
-    is_everything_disconnected =
-      Enum.all?(connections, fn pool_info ->
-        Enum.all?(pool_info[:connection_status], fn
-          status -> status == :disconnected
-        end)
-      end)
+    is_everything_disconnected = Enum.all?(connections, &is_pool_disconnected?/1)
 
     case is_everything_disconnected do
       true ->
@@ -54,5 +51,65 @@ defmodule MongoosePushWeb.HealthcheckController do
       false ->
         200
     end
+  end
+
+  defp is_pool_disconnected?(pool_info) do
+    case pool_info[:connection_status][:connected] do
+      0 ->
+        true
+
+      _ ->
+        false
+    end
+  end
+
+  # Response formatted to match the draft RFC for healthcheck endpoints, described here:
+  # https://tools.ietf.org/id/draft-inadarei-api-health-check-01.html
+  defp format_response(status, connections) do
+    {_, pool_infos} =
+      connections
+      |> Enum.map_reduce(%{}, &format_pool_info/2)
+
+    health =
+      case status do
+        503 ->
+          "fail"
+
+        200 ->
+          "pass"
+      end
+
+    %{
+      status: health,
+      version: "2",
+      releaseID: "2.0.2",
+      description: "Health of MongoosePush connections to FCM and APNS services",
+      details: pool_infos
+    }
+  end
+
+  defp format_pool_info(pool_info, acc) do
+    status =
+      case is_pool_disconnected?(pool_info) do
+        true ->
+          "fail"
+
+        false ->
+          "pass"
+      end
+
+    pool_name = "pool:#{pool_info[:pool_name]}"
+
+    pool_details = [
+      %{
+        status: status,
+        time: DateTime.utc_now(),
+        output: pool_info[:connection_status]
+      }
+    ]
+
+    new_acc = Map.put(acc, pool_name, pool_details)
+
+    {pool_info, new_acc}
   end
 end
