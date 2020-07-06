d2763d9b54a05772c11e6de09664d9e4f2291806
zofpolkowska <zofpolkowska@gmail.com>
2020-07-06 15:01:29 +0200

    Add versions.js file extension to gh_pages_docs mix task

--- lib/mix/tasks/gh_pages_docs.ex
+++ lib/mix/tasks/gh_pages_docs.ex
@@ -10,36 +10,59 @@ defmodule Mix.Tasks.GhPagesDocs do
   @shortdoc "Update a documentation version on GH Pages."
 
   @spec run(term) :: :ok
-  def run(_args) do
-    version = "v#{Project.config()[:version]}"
+  def run([version]) do
+    unless File.read!("assets/js/versions.js") |> String.contains?(version) do
+      update_versions_js()
+    end
 
-    case File.read!("assets/js/versions.js") |> String.contains?(version) do
-      true ->
-        Mix.Task.run("docs")
-        File.cd!("doc")
-        Mix.shell().cmd("git stash")
+    Mix.Task.run("docs")
+    File.cd!("doc")
+    Mix.shell().cmd("git stash")
 
-        Mix.shell().cmd("git checkout gh-pages")
+    Mix.shell().cmd("git checkout gh-pages")
 
-        case File.mkdir("../#{version}") do
-          result when result in [:ok, {:error, :eexist}] ->
-            Mix.shell().cmd("cp -r ./* ../#{version}")
-            File.cd!("..")
-            Mix.shell().cmd("ls")
-            Mix.shell().cmd("git add #{version}/*")
-            Mix.shell().cmd("git commit -m \"Update #{version}\"")
-            Mix.shell().cmd("rm -rf doc")
-            Mix.shell().cmd("git push origin gh-pages")
+    case File.mkdir("../#{version}") do
+      result when result in [:ok, {:error, :eexist}] ->
+        Mix.shell().cmd("cp -r ./* ../#{version}")
+        File.cd!("..")
+        Mix.shell().cmd("ls")
+        Mix.shell().cmd("git add #{version}/*")
 
-          _ ->
-            Mix.raise("Cannot create the #{version} directory")
+        unless File.read!("assets/js/versions.js") |> String.contains?(version) do
+          update_versions_js()
         end
 
+        Mix.shell().cmd("git add assets/js/versions.js")
+        Mix.shell().cmd("git commit -m \"Add content for #{version}\"")
+        Mix.shell().cmd("rm -rf doc")
+        Mix.shell().cmd("git show ")
+        Mix.shell().cmd("git push origin gh-pages")
+
       _ ->
-        Mix.raise("""
-        The assets/js/versions.js file is out of date.
-        Please add the newest version to the list.
-        """)
+        Mix.raise("Cannot create the #{version} directory")
     end
+  end
+
+  defp update_versions_js() do
+    {output, 0} = System.cmd("git", ["tag"], [])
+
+    versions =
+      output
+      |> String.split("\n")
+      |> Enum.drop(7)
+      |> Enum.drop(-1)
+      |> Enum.map(&v/1)
+      |> List.insert_at(0, "latest")
+      |> Enum.map(&version_elem/1)
+      |> Enum.join(",\n")
+
+    File.write!("assets/js/versions.js", "var versionNodes = [\n#{versions}\n]")
+  end
+
+  defp v(version), do: "v#{version}"
+
+  defp version_elem(version) do
+    "\t {\n\t\tversion: \"#{version}\",
+     \turl: \"https://esl.github.io/MongoosePush/#{version}/readme.html\"\n\t }"
   end
 end
