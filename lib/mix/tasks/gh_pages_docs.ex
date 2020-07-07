b65a6d86307124598c5a96cec2a8b79039145998
Zofia Polkowska <33136024+zofpolkowska@users.noreply.github.com>
2020-07-07 15:48:28 +0200

    Deploy documentation to GH Pages from CCI (#184)
    * Build docs on CCI
    
    * Add versions.js file extension to gh_pages_docs mix task
    
    * Unify paths
    
    * Configure GH MPush user
    
    * Manage versions correctly
    
    * Address the review comments
    
    * Match on successful return from every cmd

--- lib/mix/tasks/gh_pages_docs.ex
+++ lib/mix/tasks/gh_pages_docs.ex
@@ -10,36 +10,93 @@ defmodule Mix.Tasks.GhPagesDocs do
   @shortdoc "Update a documentation version on GH Pages."
 
   @spec run(term) :: :ok
-  def run(_args) do
-    version = "v#{Project.config()[:version]}"
+  def run([version]) do
+    unless String.contains?(File.read!("assets/js/versions.js"), version) do
+      update_versions_js()
+    end
 
-    case File.read!("assets/js/versions.js") |> String.contains?(version) do
-      true ->
-        Mix.Task.run("docs")
-        File.cd!("doc")
-        Mix.shell().cmd("git stash")
+    version =
+      case version do
+        "latest" -> Project.config()[:version]
+        tag -> "v#{tag}"
+      end
 
-        Mix.shell().cmd("git checkout gh-pages")
+    Mix.Task.run("docs")
+    File.cd!("doc")
+    0 = Mix.shell().cmd("git stash")
 
-        case File.mkdir("../#{version}") do
-          result when result in [:ok, {:error, :eexist}] ->
-            Mix.shell().cmd("cp -r ./* ../#{version}")
-            File.cd!("..")
-            Mix.shell().cmd("ls")
-            Mix.shell().cmd("git add #{version}/*")
-            Mix.shell().cmd("git commit -m \"Update #{version}\"")
-            Mix.shell().cmd("rm -rf doc")
-            Mix.shell().cmd("git push origin gh-pages")
+    # ignore all the changes, the only changes we want to track are in the
+    # doc directory which is listed in .gitignore
 
-          _ ->
-            Mix.raise("Cannot create the #{version} directory")
+    0 = Mix.shell().cmd("git checkout gh-pages")
+
+    case File.mkdir("../#{version}") do
+      result when result in [:ok, {:error, :eexist}] ->
+        0 = Mix.shell().cmd("cp -r ./* ../#{version}")
+        File.cd!("..")
+        0 = Mix.shell().cmd("git add #{version}/*")
+
+        unless String.contains?(File.read!("assets/js/versions.js"), version) do
+          update_versions_js()
         end
 
+        update_index_html()
+
+        0 = Mix.shell().cmd("git add assets/js/versions.js")
+        0 = Mix.shell().cmd("git add index.html")
+        0 = Mix.shell().cmd("git commit -m \"Add content for #{version}\"")
+        0 = Mix.shell().cmd("rm -rf doc")
+        0 = Mix.shell().cmd("git push origin gh-pages")
+
       _ ->
-        Mix.raise("""
-        The assets/js/versions.js file is out of date.
-        Please add the newest version to the list.
-        """)
+        Mix.raise("Cannot create the #{version} directory")
     end
+  end
+
+  defp update_versions_js() do
+    current = Project.config()[:version]
+    {output, 0} = System.cmd("git", ["tag"], [])
+
+    versions = [
+      current
+      # we skip the old releases and generate from 1.0.6 on
+      | String.split(output, "\n") --
+          [
+            "0.1.0",
+            "0.10.0",
+            "0.9.0",
+            "1.0.0",
+            "1.0.3",
+            "1.0.4",
+            "1.0.5"
+          ]
+    ]
+
+    versions
+    |> Enum.map(&version_elem/1)
+    |> Enum.join(",\n")
+
+    File.write!("assets/js/versions.js", "var versionNodes = [\n#{versions}\n]")
+  end
+
+  def update_index_html() do
+    current = Project.config()[:version]
+
+    content = """
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="0;url=https://esl.github.io/MongoosePush/v#{current}/readme.html" />
+          <title></title>
+      </head>
+      <body></body>
+    </html>
+    """
+
+    File.write!("index.html", content)
+  end
+
+  defp version_elem(version) do
+    "\t {\n\t\tversion: \"v#{version}\",
+     \turl: \"https://esl.github.io/MongoosePush/v#{version}/readme.html\"\n\t }"
   end
 end
