f1093323c8d614ef8b801e01822cadd522401080
mongoosepushbot <mongoosepushbot@erlang-solutions.com>
2024-03-27 11:49:23 +0000

    Add content for v2.1.1

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -1,30 +1,34 @@
 var versionNodes = [
-    {
-      version: "v2.1.0", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.1.0-alpha.1/introduction.html"
-    },
-    {
-      version: "v2.0.2", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.0.2/readme.html#content"
-    },
-    {
-      version: "v2.0.1", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.0.1/readme.html#content"
-    },
-    {
-      version: "v2.0.0-beta.1", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.0.0-beta.1/readme.html#content"
-    },
-    {
-      version: "v2.0.0-alpha.4", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.0.0-alpha.4/readme.html#content"
-    },
-    {
-      version: "v2.0.0", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v2.0.0/readme.html#content"
-    },
-    {
-      version: "v1.0.6", // version number or name (required)
-      url: "https://esl.github.io/MongoosePush/v1.0.6/readme.html#content"
-    }
+	 {
+		version: "v2.1.1",
+     	url: "https://esl.github.io/MongoosePush/v2.1.1/readme.html"
+	 },
+	 {
+		version: "v2.1.0",
+     	url: "https://esl.github.io/MongoosePush/v2.1.0/readme.html"
+	 },
+	 {
+		version: "v2.0.2",
+     	url: "https://esl.github.io/MongoosePush/v2.0.2/readme.html"
+	 },
+	 {
+		version: "v2.0.1",
+     	url: "https://esl.github.io/MongoosePush/v2.0.1/readme.html"
+	 },
+	 {
+		version: "v2.0.0-beta.1",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0-beta.1/readme.html"
+	 },
+	 {
+		version: "v2.0.0-alpha.4",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0-alpha.4/readme.html"
+	 },
+	 {
+		version: "v2.0.0",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0/readme.html"
+	 },
+	 {
+		version: "v1.0.6",
+     	url: "https://esl.github.io/MongoosePush/v1.0.6/readme.html"
+	 }
 ]
