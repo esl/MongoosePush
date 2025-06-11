854f1f376d8182d327ffd55da57b1c4875da1a1f
mongoosepushbot <mongoosepushbot@erlang-solutions.com>
2025-06-11 09:05:16 +0000

    Add content for v2.3.0-dev

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -1,7 +1,7 @@
 var versionNodes = [
 	 {
-		version: "v2.2.0-dev",
-     	url: "https://esl.github.io/MongoosePush/v2.2.0-dev/readme.html"
+		version: "v2.3.0-dev",
+     	url: "https://esl.github.io/MongoosePush/v2.3.0-dev/readme.html"
 	 },
 	 {
 		version: "v2.2.0",
