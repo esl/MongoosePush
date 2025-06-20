a11a0a6aebeaff7357dcffe4749fd86cc7cd536d
mongoosepushbot <mongoosepushbot@erlang-solutions.com>
2025-06-20 08:19:36 +0000

    Add content for v2.3.0-dev

---
+++ v2.0.0/docs_config.js
@@ -0,0 +1,46 @@
+var versionNodes = [
+	 {
+		version: "v2.3.0-dev",
+     	url: "https://esl.github.io/MongoosePush/v2.3.0-dev/readme.html"
+	 },
+	 {
+		version: "v2.2.0",
+     	url: "https://esl.github.io/MongoosePush/v2.2.0/readme.html"
+	 },
+	 {
+		version: "v2.1.2",
+     	url: "https://esl.github.io/MongoosePush/v2.1.2/readme.html"
+	 },
+	 {
+		version: "v2.1.1",
+     	url: "https://esl.github.io/MongoosePush/v2.1.1/readme.html"
+	 },
+	 {
+		version: "v2.1.0",
+     	url: "https://esl.github.io/MongoosePush/v2.1.0/readme.html"
+	 },
+	 {
+		version: "v2.0.2",
+     	url: "https://esl.github.io/MongoosePush/v2.0.2/readme.html"
+	 },
+	 {
+		version: "v2.0.1",
+     	url: "https://esl.github.io/MongoosePush/v2.0.1/readme.html"
+	 },
+	 {
+		version: "v2.0.0-beta.1",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0-beta.1/readme.html"
+	 },
+	 {
+		version: "v2.0.0-alpha.4",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0-alpha.4/readme.html"
+	 },
+	 {
+		version: "v2.0.0",
+     	url: "https://esl.github.io/MongoosePush/v2.0.0/readme.html"
+	 },
+	 {
+		version: "v1.0.6",
+     	url: "https://esl.github.io/MongoosePush/v1.0.6/readme.html"
+	 }
+]
