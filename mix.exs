a8ee3c8889bcdb1490146a692e181914f9f3ff5d
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2026-09-21 15:27:26 +0200

    Use sparrow 1.2.0

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule MongoosePush.Mixfile do
   defp deps do
     [
       {:chatterbox, github: "joedevivo/chatterbox", ref: "c0506c70", override: true},
-      {:sparrow, github: "esl/sparrow", branch: "audio-video-calls"},
+      {:sparrow, github: "esl/sparrow", tag: "1.2.0"},
       {:plug, "~> 1.20"},
       {:plug_cowboy, "~> 2.9"},
       {:cowboy_telemetry, "~> 0.4.0", override: true},
